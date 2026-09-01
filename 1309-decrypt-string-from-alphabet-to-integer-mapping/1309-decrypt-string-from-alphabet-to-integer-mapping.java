7acbabdb7d84fc7765894c67ac94340bf121805a
PRAMOD V <pramodv9988@gmail.com>
2026-09-01 18:01:43 +0530

    Time: 1 ms (95.36%), Space: 42.9 MB (75.56%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -1,32 +1,22 @@
 class Solution {
     public String freqAlphabets(String s) {
-        s=s.replace("10#","j");
-        s=s.replace("11#","k");
-        s=s.replace("12#","l");
-        s=s.replace("13#","m");
-        s=s.replace("14#","n");
-        s=s.replace("15#","o");
-        s=s.replace("16#","p");
-        s=s.replace("17#","q");
-        s=s.replace("18#","r");
-        s=s.replace("19#","s");
-        s=s.replace("20#","t");
-        s=s.replace("21#","u");
-        s=s.replace("22#","v");
-        s=s.replace("23#","w");
-        s=s.replace("24#","x");
-        s=s.replace("25#","y");
-        s=s.replace("26#","z");
-        s=s.replace("1","a");
-        s=s.replace("2","b");
-        s=s.replace("3","c");
-        s=s.replace("4","d");
-        s=s.replace("5","e");
-        s=s.replace("6","f");
-        s=s.replace("7","g");
-        s=s.replace("8","h");
-        s=s.replace("9","i");
-
-        return s;
+        int i = s.length()-1;
+        StringBuilder res = new StringBuilder();
+        while(i>=0)
+        {
+            if(s.charAt(i)=='#')
+            {
+                int num = (s.charAt(i-2)-'0')*10 + (s.charAt(i-1)-'0');
+                res.append((char) (96+num));
+                i-=3;
+            }
+            else
+            {
+                int num = s.charAt(i)-'0';
+                res.append((char) (96+num));
+                i--;
+            }
+        }
+        return res.reverse().toString();
     }
 }
