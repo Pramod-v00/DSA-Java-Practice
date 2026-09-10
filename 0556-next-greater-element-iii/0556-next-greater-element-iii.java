2d9b9370b1bf6b14da8693e164c8992ad2db8ede
PRAMOD V <pramodv9988@gmail.com>
2026-09-10 20:44:49 +0530

    Time: 0 ms (100%), Space: 42.2 MB (41.29%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.java
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -4,27 +4,21 @@ class Solution {
         char[] a=(String.valueOf(n)).toCharArray();
         int j=a.length-2;
         boolean flag=true;
-        while(j>=0){
-            i=a.length-1;
-            if(a[j]<a[j+1]){
-                while(i>=j){
-                    if(a[i]>a[j]){
-                        swap(a,i,j);
-                        break;
-                    }
-                    i--;
-                }
-                rev(a, j+1,a.length-1);
-                flag=false;
-            }
+        while(j>=0 && a[j]>=a[j+1]){
             j--;
-            if(!flag){
-                long ans = Long.parseLong(new String(a));
-                if(ans>Integer.MAX_VALUE) return -1;
-                return (int)ans;
-            } 
         }
-        return -1;
+        if(j==-1) return -1;
+
+        i=a.length-1;
+            
+        while(i>j && a[i]<=a[j]){
+            i--;
+        }
+        swap(a,i,j);
+        rev(a, j+1,a.length-1);       
+        long ans = Long.parseLong(new String(a));
+        if(ans>Integer.MAX_VALUE) return -1;
+        return (int)ans;
     }
     void swap(char[] a,int i,int j){
         char temp=a[i];
