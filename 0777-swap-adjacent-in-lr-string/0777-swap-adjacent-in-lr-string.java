a72fe6dae813d353d8a0b335b28dd83c1535f959
PRAMOD V <pramodv9988@gmail.com>
2026-09-11 09:51:56 +0530

    Time: 4 ms (86.93%), Space: 45 MB (33.57%) - LeetHub

--- 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.java
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.java
@@ -1,27 +1,27 @@
 class Solution {
     public boolean canTransform(String start, String result) {
-        int i=0,j=0;
-        while(i<start.length() && j<result.length()){
-            if(start.charAt(i)=='X'){
-                i++;
-                continue;
+        int n = start.length();
+        int i = 0, j = 0;
+
+        while (i < n || j < n) {
+            while (i < n && start.charAt(i) == 'X') i++;
+            while (j < n && result.charAt(j) == 'X') j++;
+
+            if ((i == n) ^ (j == n)) return false;
+
+            if (i < n && j < n) {
+                char c1 = start.charAt(i);
+                char c2 = result.charAt(j);
+
+                if (c1 != c2) return false;
+                if (c1 == 'L' && j > i) return false;
+                if (c1 == 'R' && j < i) return false;
             }
-            if(result.charAt(j)=='X'){
-                j++;
-                continue;
-            }
-            if(start.charAt(i)==result.charAt(j)){
-                if(start.charAt(i)=='L' && i < j)
-                    return false;
-                if(start.charAt(i)=='R' && i > j)
-                    return false;
-                i++;
-                j++;
-                continue;
-            }else return false;
+
+            i++;
+            j++;
         }
-        while(i<start.length() && start.charAt(i)=='X')i++;
-        while(j<result.length() && result.charAt(j)=='X')j++;
-        return i==j;
+
+        return true;
     }
 }
