b1de1bc2e8b53feb3764775453dab75feca0e189
PRAMOD V <pramodv9988@gmail.com>
2026-09-16 09:41:16 +0530

    Time: 2 ms (99.78%), Space: 43.8 MB (16.64%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,67 +1,31 @@
 class Solution {
-
     public String multiply(String num1, String num2) {
-
+        if(num1.equals("0") || num2.equals("0"))
+        {
+            return "0";
+        }
         int n1 = num1.length();
         int n2 = num2.length();
-
-        // If either number is 0
-        if(num1.equals("0") || num2.equals("0"))
-            return "0";
-
-        if(n1 < n2)
-            return mult(num2, num1);
-
-        return mult(num1, num2);
-    }
-
-    String mult(String m1, String m2) {
-
-        int n1 = m1.length();
-        int n2 = m2.length();
-
-        String[] temp = new String[n2];
-
-        for(int i = n2 - 1; i >= 0; i--) {
-
-            StringBuilder sb = new StringBuilder();
-            int carry = 0;
-            for(int j = n1 - 1; j >= 0; j--) {
-                int product =(m2.charAt(i) - '0') * (m1.charAt(j) - '0') + carry;
-                sb.append(product % 10);
-                carry = product / 10;
+        int[] res = new int[n1+n2];
+        for(int i=n1-1;i>=0;i--)
+        {
+            int d1 = num1.charAt(i)-'0';
+            for(int j = n2-1;j>=0;j--)
+            {
+                int d2 = num2.charAt(j)-'0';
+                int sum = d1*d2+res[i+j+1];
+                res[i+j+1] = sum%10;
+                res[i+j] += sum/10;
             }
-            if(carry > 0)
-                sb.append(carry);
-            sb.reverse();
-            for(int k = 0; k < n2 - 1 - i; k++)
-                sb.append('0');
-            temp[n2 - 1 - i] = sb.toString();
         }
-        return add(temp);
-    }
-    String add(String[] temp) {
-        String result = "0";
-        for(String str : temp) {
-            if(str != null)
-                result = addTwo(result, str);
-        }
-        return result;
-    }
-    String addTwo(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        int carry = 0;
-        while(i >= 0 || j >= 0 || carry != 0) {
-            int sum = carry;
-            if(i >= 0)
-                sum += a.charAt(i--) - '0';
-            if(j >= 0)
-                sum += b.charAt(j--) - '0';
-            sb.append(sum % 10);
-            carry = sum / 10;
-        }
-        return sb.reverse().toString();
+          StringBuilder sb = new StringBuilder();
+            for(int num: res)
+            {
+                if(sb.length() ==0 && num==0){
+                   continue;
+                }
+                sb.append(num);
+            }
+            return sb.toString();
     }
 }
