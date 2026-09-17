daeab2db826d4031af5bfe9170101a6959e567df
PRAMOD V <pramodv9988@gmail.com>
2026-09-17 22:57:51 +0530

    Time: 5 ms (98.1%), Space: 71.6 MB (89.42%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.java
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -1,15 +1,22 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
-        StringBuilder sb=new StringBuilder(s);
-        long sum=0;
-        for(int i=0;i<shifts.length;i++){
-            sum+=shifts[i];
+        // StringBuilder sb=new StringBuilder(s);
+        // long sum=0;
+        // for(int i=0;i<shifts.length;i++){
+        //     sum+=shifts[i];
+        // }
+        // for(int i=0;i<shifts.length;i++){
+        //     char temp=(char)((sb.charAt(i) - 'a' + sum) % 26 + 'a');
+        //     sb.setCharAt(i,temp);
+        //     sum-=shifts[i];
+        // }
+        // return sb.toString();
+        char[] ans=s.toCharArray();
+        long tot=0;
+        for(int i=shifts.length-1;i>=0;i--){
+            tot+=shifts[i];
+            ans[i]=(char)((ans[i]-'a' + tot)%26 +'a');
         }
-        for(int i=0;i<shifts.length;i++){
-            char temp=(char)((sb.charAt(i) - 'a' + sum) % 26 + 'a');
-            sb.setCharAt(i,temp);
-            sum-=shifts[i];
-        }
-        return sb.toString();
+        return new String(ans);
     }
 }
